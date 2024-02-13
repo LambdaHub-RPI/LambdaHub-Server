83c621625937d4acdc3344e26c5ae1e390fb032c
Nevin Joshy <joshyn@rpi.edu>
2024-02-13 17:47:26 -0500

    implemented initial backend for calendar app

--- lambdahub/calendar_app/models.py
+++ lambdahub/calendar_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    date = models.DateField()
+    starttime = models.TimeField()
+    endtime = models.TimeField()
+
+    def __str__(self):
+        return str(self.title)
+    
+    def save(self, *args, **kwargs):
+        super().save(*args,**kwargs)
